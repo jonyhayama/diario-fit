3fcf18e44721e7d8d4b6d4ef2b883eab63663cbb
Jony Hayama <jonathan.hayama@gmail.com>
2021-06-17 15:08:08 -0300

    🏗️ Add user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
   has_secure_password
+
+  PASSWORD_REGEX = /\A(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,70}\Z/.freeze
+  PASSWORD_COMPLEXITY = 'must be over 6 characters and include: ' \
+                        '1 uppercase, 1 lowercase, 1 digit and 1 special character'
+
+  validates :email, uniqueness: true, presence: true
+  validates :password,
+            format: { with: PASSWORD_REGEX, message: PASSWORD_COMPLEXITY },
+            if: :will_save_change_to_password_digest?
+  validates :password_confirmation, presence: true, if: :will_save_change_to_password_digest?
 end
