e1063331e27250b0e0a2f657742abfa41501fd53
Jony Hayama <jonathan.hayama@gmail.com>
2021-06-21 14:09:52 -0300

    🏗️ Move authentication endpoints to api namespace

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     root to: 'home#index'
+    post 'refresh', controller: :refresh, action: :create
+    post 'signup', controller: :signup, action: :create
+    post 'signin', to: 'signin#create'
+    delete 'signin', to: 'signin#destroy'
+    
     namespace :v1 do
       get 'me', to: 'users#me'
     end
   end
-
-  post 'refresh', controller: :refresh, action: :create
-  post 'signup', controller: :signup, action: :create
-  post 'signin', to: 'signin#create'
-  delete 'signin', to: 'signin#destroy'
 
   get '/*path', to: 'static_pages#index'
 end
