288127476a2488600f6d64ed88ab8867bea54715
Jony Hayama <jonathan.hayama@gmail.com>
2021-06-25 14:59:19 -0300

    ✨ Create dashboard home component

---
+++ frontend/src/views/Dashboard.vue
@@ -0,0 +1,18 @@
+<template>
+  <p>Hello {{ currentUser.email }}! you're logged in! <button @click="signOut">Sign out</button></p>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+export default {
+  computed: {
+    ...mapGetters('authentication', ['currentUser']),
+  },
+  methods: {
+    async signOut() {
+      await this.$store.dispatch('authentication/signOut');
+      this.$router.push({ path: '/' });
+    }
+  }
+}
+</script>
